58e7daf41ff2746febf136e2316629afb4a5d1ff
liuwang dell <1183862787@qq.com>
2020-04-29 07:24:19 +0800

    the dataset class mapping to training, evaluating, and testing data.

--- prep_result.py
+++ prep_result.py
@@ -21,7 +21,7 @@ def restoration(udc):
     data_batch = from_numpy(np.array([udc]).transpose((0, 3, 1, 2))).float().to(DEVICE)
     model = Restorer(image_c=3, N=64).to(DEVICE)
     model.load_state_dict(torch.load(
-        f='pkls/model_1441.pkl',
+        f='pkls/model_38339.pkl',
         map_location=DEVICE))
     with torch.no_grad():
         output_batch = model(data_batch).cpu().numpy()
