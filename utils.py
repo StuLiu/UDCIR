6de34d6a00be8fad3e8321c3b9913bd7f3542bdf
liuwang dell <1183862787@qq.com>
2020-05-11 09:08:45 +0800

    the dataset class mapping to training, evaluating, and testing data.

--- utils.py
+++ utils.py
@@ -39,5 +39,5 @@ if __name__ == '__main__':
 	# show_mat(mat_file_path='./toled_val_display.mat', key='val_display')
 	# show_mat(mat_file_path='./res_dir/results.mat', key='results')
 	# show_mat(mat_file_path='./results_dir/results.mat', key='results')
-	print(compute_PSNR(loadmat('./toled_val_display.mat')['results'],
+	print(compute_PSNR(loadmat('./toled_val_display.mat')['val_display'],
 	                   loadmat('./res_dir/results.mat')['results']))
