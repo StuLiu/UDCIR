d985b6f16ecbef1e36fcc6224e9716deea754a73
liuwang dell <1183862787@qq.com>
2020-05-12 11:01:02 +0800

    the dataset class mapping to training, evaluating, and testing data.

--- preprocess.py
+++ preprocess.py
@@ -63,10 +63,10 @@ def _image_enhance(img, only_crop=False)->list:
 	for img_ in imgs_cropped:
 		if not only_crop:
 			# img_flipped_y = cv2.flip(img_, 1)         # flip the image around y-axis
-			# img_flipped_x = cv2.flip(img_, 0)         # flip the image around x-axis
+			img_flipped_x = cv2.flip(img_, 0)         # flip the image around x-axis
 			img_flipped_xy = cv2.flip(img_, -1)         # flip the image around x-axis
-			img_transposed = cv2.transpose(img)         # transpose the image
-			results.extend([img_, img_flipped_xy, img_transposed])
+			# img_transposed = cv2.transpose(img)         # transpose the image
+			results.extend([img_, img_flipped_xy, img_flipped_x])
 		else:
 			results.append(img_)
 	# print(np.array(results).shape)
