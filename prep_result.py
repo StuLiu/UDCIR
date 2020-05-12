87436e32d38e7bbfc4492a308bf5e6fa0a3d73f9
liuwang dell <1183862787@qq.com>
2020-05-12 21:33:10 +0800

    the dataset class mapping to training, evaluating, and testing data.

--- prep_result.py
+++ prep_result.py
@@ -6,17 +6,30 @@ import os.path
 import shutil
 import torch
 from scipy.io.matlab.mio import savemat, loadmat
-from model import UNet
+from model import UNet, CNN
 from torch import from_numpy
 import sys
 # import cv2
 # from preprocess import image_crop, image_splice
 
 print('begin')
-model_path = sys.argv[1]
-dev = sys.argv[2]
+model_name = sys.argv[1]
+pkl_path = sys.argv[2]
+dev = sys.argv[3]
 DEVICE = torch.device("cuda" if torch.cuda.is_available() and dev=='cuda' else "cpu")
-print(model_path, DEVICE)
+print(model_name, pkl_path, DEVICE)
+if model_name == 'UNet-16':
+    model = UNet(N=16)
+elif model_name == 'UNet-32':
+    model = UNet(N=32)
+elif model_name == 'UNet-64':
+    model = UNet(N=64)
+elif model_name == 'CNN':
+    model = CNN(N=32)
+else:
+    model = None
+    print('>>> Model name:{} invalid!')
+    exit(-1)
 
 def restoration(udc, model):
     # TODO: plug in your method here
@@ -33,38 +46,19 @@ def restoration(udc, model):
     # cv2.imshow('img', result)
     # cv2.waitKey(0)
     return result
-#
-# def restoration(udc, model):
-#     # TODO: plug in your method here
-#     print('udc.shape', udc.shape)   # (h, w, c)
-#     cropped_imgs = np.array(image_crop(udc))
-#     data_batch = from_numpy(cropped_imgs.transpose((0, 3, 1, 2))).float().to(DEVICE)
-#     with torch.no_grad():
-#         output_batch = model(data_batch).cpu().numpy()
-#         output_batch = np.where(output_batch < 0, 0, output_batch)
-#         output_batch = np.where(output_batch > 255, 255, output_batch)
-#     enhanced_imgs = output_batch.transpose((0, 2, 3, 1))
-#     enhanced_img = image_splice(enhanced_imgs)
-#     print('result.shape', enhanced_img.shape)
-#     # cv2.imshow('img', enhanced_img)
-#     # cv2.waitKey(0)
-#     return enhanced_img
 
 # TODO: update your working directory; it should contain the .mat file containing noisy images
 work_dir = './'
 
 # load model
-model = UNet(N=32).to(DEVICE)
-model.load_state_dict(torch.load(
-    f=os.path.join(work_dir, model_path),
-    map_location=DEVICE)
-)
+model.load_state_dict(torch.load(f=pkl_path, map_location=DEVICE))
 
 # load noisy images
 udc_fn = 'toled_val_display.mat'  # or poled_val_display.mat
 udc_key = 'val_display'
 udc_mat = loadmat(os.path.join(work_dir, udc_fn))[udc_key]
 print(type(udc_mat), udc_mat[0,0:5,0:5,:])
+
 # restoration
 n_im, h, w, c = udc_mat.shape
 results = udc_mat.copy()
@@ -74,6 +68,7 @@ for i in range(n_im):
     results[i, :, :, :] = restored
 print(type(results), results[0,0:5,0:5,:])
 exit(0)
+
 # create results directory
 res_dir = 'res_dir'
 os.makedirs(os.path.join(work_dir, res_dir), exist_ok=True)
@@ -102,3 +97,18 @@ with open(readme_fn, 'w') as readme_file:
 res_zip_fn = 'results_dir'
 shutil.make_archive(os.path.join(work_dir, res_zip_fn), 'zip', os.path.join(work_dir, res_dir))
 print('finished')
+
+# def restoration(udc, model):
+#     print('udc.shape', udc.shape)   # (h, w, c)
+#     cropped_imgs = np.array(image_crop(udc))
+#     data_batch = from_numpy(cropped_imgs.transpose((0, 3, 1, 2))).float().to(DEVICE)
+#     with torch.no_grad():
+#         output_batch = model(data_batch).cpu().numpy()
+#         output_batch = np.where(output_batch < 0, 0, output_batch)
+#         output_batch = np.where(output_batch > 255, 255, output_batch)
+#     enhanced_imgs = output_batch.transpose((0, 2, 3, 1))
+#     enhanced_img = image_splice(enhanced_imgs)
+#     print('result.shape', enhanced_img.shape)
+#     # cv2.imshow('img', enhanced_img)
+#     # cv2.waitKey(0)
+#     return enhanced_img
