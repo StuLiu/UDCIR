2ada7b2d3b775857ad81c10a8c5e725a55781bc9
liuwang dell <1183862787@qq.com>
2020-05-12 10:57:05 +0800

    the dataset class mapping to training, evaluating, and testing data.

--- preprocess.py
+++ preprocess.py
@@ -17,7 +17,7 @@ import numpy as np
 from scipy.io import loadmat
 from tqdm import tqdm
 
-def read_imgs_from_dir(img_dir_path='data/Train/Toled/HQ', enhance=True)->np.ndarray:
+def read_imgs_from_dir(img_dir_path='data/Train/Toled/HQ', enhance=True, only_crop=False)->np.ndarray:
 	print('>>> read images from {}. <<<'.format(img_dir_path))
 	if not os.path.exists(img_dir_path):
 		raise Exception(" No such file or directory:{0}".format(img_dir_path))
@@ -28,7 +28,7 @@ def read_imgs_from_dir(img_dir_path='data/Train/Toled/HQ', enhance=True)->np.nda
 	for path in tqdm(img_paths):
 		img_data = cv2.imread(path)    # return the ndarray of image, [high, width, channal(3)]
 		if enhance:
-			img_data_list.extend(_image_enhance(img_data))
+			img_data_list.extend(_image_enhance(img_data, only_crop=only_crop))
 		else:
 			img_data_list.extend([img_data])
 	return np.array(img_data_list)
@@ -56,21 +56,27 @@ def _image_rotate(img)->list:
 	img_cw_270 = cv2.flip(cv2.transpose(img), 0)
 	return [img, img_cw_90, img_cw_180, img_cw_270]
 
-def _image_enhance(img)->list:
+def _image_enhance(img, only_crop=False)->list:
 	imgs_cropped = _image_crop(img)
 	# return imgs_cropped
 	results = []
 	for img_ in imgs_cropped:
-		# img_flipped_y = cv2.flip(img_, 1)         # flip the image around y-axis
-		# img_flipped_x = cv2.flip(img_, 0)         # flip the image around x-axis
-		img_flipped_xy = cv2.flip(img_, -1)         # flip the image around x-axis
-		results.extend([img_, img_flipped_xy])
+		if not only_crop:
+			# img_flipped_y = cv2.flip(img_, 1)         # flip the image around y-axis
+			# img_flipped_x = cv2.flip(img_, 0)         # flip the image around x-axis
+			img_flipped_xy = cv2.flip(img_, -1)         # flip the image around x-axis
+			img_transposed = cv2.transpose(img)         # transpose the image
+			results.extend([img_, img_flipped_xy, img_transposed])
+		else:
+			results.append(img_)
 	# print(np.array(results).shape)
 	return results
 
-def _imgs2npy(img_dir_path, enhance, out_path):
-	imgs = read_imgs_from_dir(img_dir_path, enhance=enhance)    # (N, h, w, c)
-	imgs = np.transpose(imgs, (0, 3, 1, 2))                     # (N, c, h, w)
+def _imgs2npy(img_dir_path, out_path, enhance, only_crop):
+	imgs = read_imgs_from_dir(img_dir_path,
+	                          enhance=enhance,
+	                          only_crop=only_crop)      # (N, h, w, c)
+	imgs = np.transpose(imgs, (0, 3, 1, 2))             # (N, c, h, w)
 	print(imgs.shape)
 	np.save(out_path, imgs)
 	# cv2.imshow('HQ', np.transpose(HQ[22], (1, 2, 0)))
@@ -84,10 +90,22 @@ if __name__ == '__main__':
 	# print(read_imgs_from_mat('toled_val_display.mat').shape)
 	# cv2.imshow('LQ', read_imgs_from_mat('toled_val_display.mat')[22])
 	# cv2.waitKey(0)
-	_imgs2npy(img_dir_path='data/Train/Toled/HQ', enhance=True, out_path='data/Train/Toled/HQ_256.npy')
-	_imgs2npy(img_dir_path='data/Train/Toled/LQ', enhance=True, out_path='data/Train/Toled/LQ_256.npy')
-	_imgs2npy(img_dir_path='data/Eval/Toled/HQ', enhance=True, out_path='data/Eval/Toled/HQ_256.npy')
-	_imgs2npy(img_dir_path='data/Eval/Toled/LQ', enhance=True, out_path='data/Eval/Toled/LQ_256.npy')
+	_imgs2npy(img_dir_path='data/Train/Toled/HQ',
+	          out_path='data/Train/Toled/HQ_256.npy',
+	          enhance=True,
+	          only_crop=False)
+	_imgs2npy(img_dir_path='data/Train/Toled/LQ',
+	          out_path='data/Train/Toled/LQ_256.npy',
+	          enhance=True,
+	          only_crop=False)
+	_imgs2npy(img_dir_path='data/Eval/Toled/HQ',
+	          out_path='data/Eval/Toled/HQ_256.npy',
+	          enhance=True,
+	          only_crop=True)
+	_imgs2npy(img_dir_path='data/Eval/Toled/LQ',
+	          out_path='data/Eval/Toled/LQ_256.npy',
+	          enhance=True,
+	          only_crop=True)
 
 
 
